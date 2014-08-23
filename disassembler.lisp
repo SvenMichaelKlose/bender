ecdb3b162d5714b4510afef282c418b276e038a8
Sven Michael Klose <pixel@copei.de>
2014-08-23 14:55:42 +0100

    Cleaned up.

--- disassembler.lisp
+++ disassembler.lisp
@@ -22,26 +22,32 @@
   (& (not (eq 'ill (addrmode opcode)))
      (elt (elt (elt *legal-addrmodes* (opcode-cc opcode)) (opcode-bb opcode)) (opcode-aa opcode))))
 
+(defun opcode-branch? (opcode)
+  (eq 'branch (addrmode opcode)))
+
+(defun zero-aacc? (opcode)
+  (& (zero? (opcode-aa opcode))
+     (zero? (opcode-cc opcode))))
+
+(defun zero-9f? (opcode)
+  (zero? (bit-and opcode #x9f)))
+
+(defun opcode-mnemonic-addrmode (opcode)
+  (?
+    (== #x20 opcode)        (values 'jsr 'abs)
+    (== #x6c opcode)        (values 'jmp 'indi)
+    (zero-9f? opcode)       (values (opcode-mnemonic-aa-cc (>> opcode 5) 6)
+                                    nil)
+    (opcode-branch? opcode) (values (branch-mnemonic opcode)
+                                    (addrmode opcode))
+    (| (zero-aacc? opcode)
+       (not (legal-addrmode? opcode)))
+                            (values (single-mnemonic opcode) nil)
+    (values (opcode-mnemonic opcode)
+            (addrmode opcode))))
+
 (defun opcode-instruction (opcode)
-  (with (mnem  (opcode-mnemonic opcode)
-         m     (addrmode opcode))
-    (?
-      (== #x20 opcode)
-        (= m     'abs
-           mnem  'jsr)
-      (== #x6c opcode)
-        (= m     'indi
-           mnem  'jmp)
-      (zero? (bit-and opcode #x9f))
-        (= m     nil
-           mnem  (opcode-mnemonic-aa-cc (>> opcode 5) 6))
-      (eq 'branch m)
-        (= mnem (branch-mnemonic opcode))
-      (| (& (zero? (opcode-aa opcode))
-            (zero? (opcode-cc opcode)))
-         (not (legal-addrmode? opcode)))
-        (= m     nil
-           mnem  (single-mnemonic opcode)))
+  (with ((mnem m) (opcode-mnemonic-addrmode opcode))
     (make-instruction :opcode    opcode
                       :mnemonic  mnem
                       :addrmode  (?
