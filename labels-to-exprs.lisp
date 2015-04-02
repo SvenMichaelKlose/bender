7fb4fce30c23c4d6cf970e212d17f56560ef045d
Sven Michael Klose <pixel@copei.de>
2015-04-02 20:01:31 +0200

    Lookup prefixed labels properly.

--- labels-to-exprs.lisp
+++ labels-to-exprs.lisp
@@ -1,15 +1,17 @@
 ; bender – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
 (defun labels-to-exprs (x)
-  (with (f [? (get-label _ :required? nil)
-              (let n (symbol-name _)
-                (alet (make-symbol (subseq n 1))
-                  (case (elt n 0) :test #'==
-                    #\-  `(get-previous-label ,!)
-                    #\+  `(get-next-label ,!)
-                    #\<  `(low ,(f !))
-                    #\>  `(low ,(f !))
-                    `(get-label ,_))))
+  (with (f [| (? (get-label _ :required? nil)
+                 `(get-label ',_)
+                 (let n (symbol-name _)
+                   (when (< 1 (length n))
+                     (alet (make-symbol (subseq n 1))
+                       (? (get-label ! :required? nil)
+                          (case (elt n 0) :test #'==
+                            #\-  `(get-previous-label ',!)
+                            #\+  `(get-next-label ',!)
+                            #\<  `(low ,(f !))
+                            #\>  `(low ,(f !))))))))
               _])
     (when x
       (? (atom x)
