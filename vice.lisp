a17078f80fc15e575c3382806927378e76f71f7f
Sven Michael Klose <pixel@copei.de>
2015-04-14 22:10:30 +0200

    Fixed.

--- vice.lisp
+++ vice.lisp
@@ -1,7 +1,7 @@
-;;;;; bender – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
+; bender – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
 (defun make-vice-commands (out-name)
   (format t "Writing VICE monitor commands to '~A'.~%" out-name)
   (with-output-file o out-name
-    (adolist ((hashkeys *labels*))
-      (format o "al ~A .~A~%" (print-hexword (href *labels* !) nil) !))))
+    (adolist ((+ (reverse *previous-labels*) *next-labels*))
+      (format o "al ~A .~A~%" (print-hexword .! nil) !.))))
