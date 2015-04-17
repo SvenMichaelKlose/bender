7c0925e5abb682dc19789420068b996e335950ff
Sven Michael Klose <pixel@copei.de>
2015-04-17 18:37:55 +0200

    Add comment on how to use the generated file.

--- src/vice.lisp
+++ src/vice.lisp
@@ -3,5 +3,6 @@
 (defun make-vice-commands (out-name)
   (format t "Writing VICE monitor commands to '~A'.~%" out-name)
   (with-output-file o out-name
+    (format o "; For VICE with command line option -moncommands ~A" out-name)
     (adolist ((+ (reverse *previous-labels*) *next-labels*))
       (format o "al ~A .~A~%" (print-hexword .! nil) !.))))
