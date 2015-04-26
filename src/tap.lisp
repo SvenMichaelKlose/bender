1abaa7785ca5324244e9722b665d7a05336dac11
Sven Michael Klose <pixel@copei.de>
2015-04-26 15:29:00 +0200

    Cleaned up.

--- src/tap.lisp
+++ src/tap.lisp
@@ -1,16 +1,16 @@
-;;;;; bender – Copyright (c) 2014 Sven Michael Klose <pixel@copei.de>
+; bender – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
 (defun write-tap (o pulses)
   (with-queue q
-    (with (size    (length pulses))
+    (alet (length pulses)
       (format t "Writing TAP file...~%")
       (format o "C64-TAPE-RAW")
       (princ (code-char 1) o)
       (adotimes 3
         (princ (code-char 0) o))
-      (princ (code-char (mod size 256)) o)
-      (princ (code-char (mod (>> size 8) 256)) o)
-      (princ (code-char (mod (>> size 16) 256)) o)
-      (princ (code-char (mod (>> size 24) 256)) o)
-      (format t "Writing ~A pulses...~%" size)
+      (princ (code-char (mod ! 256)) o)
+      (princ (code-char (mod (>> ! 8) 256)) o)
+      (princ (code-char (mod (>> ! 16) 256)) o)
+      (princ (code-char (mod (>> ! 24) 256)) o)
+      (format t "Writing ~A pulses...~%" !)
       (princ pulses o))))
