d3d74726bee1ae6d75f78e68991e20fbd30e0777
Sven Michael Klose <pixel@copei.de>
2015-04-26 15:27:12 +0200

    CBM BASIC keywords added.

---
+++ src/cbm-basic.lisp
@@ -0,0 +1,8 @@
+(defvar +cbm-basic-keywords+
+  '("END" "FOR" "NEXT" "DATA" "INPUT#" "INPUT" "DIM" "READ" "LET" "GOTO"
+    "RUN" "IF" "RESTORE" "GOSUB" "RETURN" "REM" "STOP" "ON" "WAIT"
+    "LOAD" "SAVE" "VERIFY" "DEF" "POKE" "PRINT#" "PRINT" "CONT" "LIST"
+    "CLR" "CMD" "SYS" "OPEN" "CLOSE" "GET" "NEW" "TAB(" "TO" "FN" "SPC("
+    "THEN" "NOT" "STEP" "+" "-" "*" "/" "^" "AND" "OR" ">" "=" "<" "SGN"
+    "INT" "ABS" "USR" "FRE" "POS" "SQR" "RND" "LOG" "EXP" "COS" "SIN"
+    "PEEK" "LEN" "STR$" "VAL" "ASC" "CHR$" "LEFT$" "RIGHT$" "MID$" "GO"))
