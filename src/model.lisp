d941493a1992abc9fbb79ae934f089cfb22e7edc
Sven Michael Klose <pixel@copei.de>
2015-05-17 15:10:03 +0200

    Added BASIC start addresses for all missing machines.

--- src/model.lisp
+++ src/model.lisp
@@ -6,8 +6,14 @@
 (defun basic-start-address (model)
   (case model
     nil         *basic-start*
-    :c64        #x0801
-    :vic-20     #x1001
-    :vic-20+3k  #x0401
-    :vic-20+xk  #x1201
+    :pet         #x0401
+    :cbm         #x0401
+    :vic-20      #x1001
+    :vic-20+3k   #x0401
+    :vic-20+xk   #x1201
+    :c64         #x0801
+    :plus4       #x1001
+    :plus4hires  #x2001
+    :c128        #x1c01
+    :c128hires   #x4001
     (error "Cannot determine BASIC start address for model ~A." model)))
