d3b269e085a8982c7ea01024d45afa8b51096f2a
Sven Michael Klose <pixel@copei.de>
2015-05-18 19:37:34 +0200

    MAKE-VICE-COMMANDS got an optional argument to append own commands.

--- src/vice.lisp
+++ src/vice.lisp
@@ -1,8 +1,13 @@
 ; bender – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
-(defun make-vice-commands (out-name)
+(defun make-vice-commands (out-name &optional addition)
   (format t "Writing VICE monitor commands to '~A'.~%" out-name)
   (with-output-file o out-name
     (format o "; For VICE with command line option -moncommands ~A" out-name)
     (adolist ((+ (reverse *previous-labels*) *next-labels*))
-      (format o "al ~A .~A~%" (print-hexword .! nil) (downcase (symbol-name !.))))))
+      (format o "al ~A .~A~%"
+              (print-hexword .! nil)
+              (downcase (symbol-name !.))))
+    (awhen addition
+        (princ ! o)
+        (terpri o))))
