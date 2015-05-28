86d787c2c2407bb23a5d17b371a5d8029c14928d
Sven Michael Klose <pixel@copei.de>
2015-05-28 07:35:01 +0200

    ASCII2PETSCII: Also convert whole strings.

--- src/ascii2petscii.lisp
+++ src/ascii2petscii.lisp
@@ -1,6 +1,8 @@
 ; Bender – Copyright (c) 2015 Sven Michael Klose <pixel@hugbox.org>
 
 (defun ascii2petscii (x)
-  (? (< #\Z x)
-     (- x (-- #\a))
-     x))
+  (? (string? x)
+     (@ #'ascii2petscii (string-list x))
+     (? (< #\Z x)
+        (- x (-- #\a))
+        x)))
