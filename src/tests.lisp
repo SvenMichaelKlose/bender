8739d5784de58e56938cb9d6c55ff515fd9ba9f3
Sven Michael Klose <pixel@copei.de>
2015-06-05 20:36:28 +0200

    Disassemble to file with suffix ".lst".

--- src/tests.lisp
+++ src/tests.lisp
@@ -3,12 +3,12 @@
 (format t "; Assembling reference 'tests/all_instructions.asm' to 'out.prg'…~%")
 (assemble-files "out.prg" "tests/all_instructions.asm")
 (? (string== (fetch-file "out.prg")
-    (fetch-file "tests/all_instructions.bin"))
+             (fetch-file "tests/all_instructions.bin"))
    (format t "; All instructions assembled correctly.~%")
    (error "Instructions assembled incorrectly.~%"))
 
-(format t "; Diassembling 'out.prg' to match against 'tests/all_instructions_disassembled.asm'…")
-(disassemble-file "tests/all_instructions.bin" "all_instructions_diassembled.asm")
+(format t "; Diassembling 'out.prg' to match against 'tests/all_instructions_disassembled.lst'…")
+(disassemble-file "tests/all_instructions.bin" "all_instructions_diassembled.lst")
 (= *model* :vic-20)
 (assemble-files "obj/cbm-basic-init.prg"
                 "vic-20/basic-loader.asm"
