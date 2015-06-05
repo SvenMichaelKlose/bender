12f6a9ae402110ebf399aa08a09c05dbf87a7ae6
Sven Michael Klose <pixel@copei.de>
2015-06-05 20:43:49 +0200

    Cleaned up. All generated files are put into obj/.

--- make.sh
+++ make.sh
@@ -1,4 +1,3 @@
 rm -r obj
 mkdir obj
 tre src/main.lisp
-echo "(unless (string== (fetch-file \"tests/all_instructions.bin\") (fetch-file \"out.prg\")) (error \"Invalid test assembly!\"))" | tre
--- src/tests.lisp
+++ src/tests.lisp
@@ -1,15 +1,17 @@
 ; bender – Copyright (c) 2015 Sven Michael Klose <pixel@copei.de>
 
-(format t "; Assembling reference 'tests/all_instructions.asm' to 'out.prg'…~%")
-(assemble-files "out.prg" "tests/all_instructions.asm")
-(? (string== (fetch-file "out.prg")
+(format t "; Assembling reference 'tests/all_instructions.asm' to 'obj/all_instructions.bin'…~%")
+(assemble-files "obj/all_instructions.bin" "tests/all_instructions.asm")
+
+(format t "; Diassembling 'obj/all_instructions.bin' to 'obj/all_instructions_disassembled.lst'…")
+(disassemble-file "tests/all_instructions.bin" "obj/all_instructions_diassembled.lst")
+
+(= *model* :vic-20)
+(? (string== (fetch-file "obj/all_instructions.bin")
              (fetch-file "tests/all_instructions.bin"))
    (format t "; All instructions assembled correctly.~%")
    (error "Instructions assembled incorrectly.~%"))
 
-(format t "; Diassembling 'out.prg' to match against 'tests/all_instructions_disassembled.lst'…")
-(disassemble-file "tests/all_instructions.bin" "all_instructions_diassembled.lst")
-(= *model* :vic-20)
 (assemble-files "obj/cbm-basic-init.prg"
                 "vic-20/basic-loader.asm"
                 "growroom/cbm-basic-init.asm")
