4dd63f6cde58339a4f0ccd64f99da31f3082b257
Sven Michael Klose <pixel@copei.de>
2015-06-13 16:29:51 +0200

    Fixed data type for opcode map.

--- growroom/gen-c-disassembler.lisp
+++ growroom/gen-c-disassembler.lisp
@@ -1,22 +1,23 @@
 ; bender – Copyright (c) 2015 Sven Michael Klose <pixel@copei.de>
 
 (defun gen-c-opcode (x)
-  (+ "    "
+  (+ "    { "
      (? x.
-        (format nil "\"~A\", am_~A"
+        (format nil "\"~A\", AM_~A"
                     (downcase (symbol-name x.))
-                    (downcase (symbol-name .x)))
-        (format nil "NULL, 0"))))
+                    (symbol-name .x))
+        (format nil "NULL, 0"))
+     " }"))
 
 (defun gen-c-opcode-map ()
-  (with-output-file o "obj/intruction-map.c"
+  (with-output-file o "obj/instruction-map.c"
     (format o "struct instruction {~%")
     (format o "    const char * mnemonic;~%")
-    (format o "    char addrmode;~%")
+    (format o "    int addrmode;~%")
     (format o "} opcode_map[256] = {~%")
     (princ (apply #'+ (pad (@ #'gen-c-opcode (array-list *opcode-map*))
                            (format nil ",~%")))
            o)
-    (format o "~%}~%")))
+    (format o "~%};~%")))
 
 (gen-c-opcode-map)
