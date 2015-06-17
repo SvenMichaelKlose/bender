adb918b427bb25c2833ee7a698d268b441a33f9c
Sven Michael Klose <pixel@copei.de>
2015-06-17 12:50:36 +0200

    Fixed.

--- prepare.sh
+++ prepare.sh
@@ -4,4 +4,4 @@ echo "Installing required packages for bender with aptitude…"
 sudo aptitude install git sbcl
 echo "Cloning, building and installing git repositories required for bender…"
 git clone https://github.com/svenmichaelklose/tre --depth 1
-cd tre; sh make.sh core: sh make.sh install
+cd tre && ./make.sh core && ./make.sh install
