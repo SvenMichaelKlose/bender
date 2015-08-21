a1e2dd2d40c9e78200ef2bf1cb3a4e975f1b07ea
Sven Michael Klose <pixel@copei.de>
2015-08-21 15:03:48 +0200

    Have right start address depending on CBM model.

--- vic-20/basic-loader.asm
+++ vic-20/basic-loader.asm
@@ -1,6 +1,10 @@
     org @(- (basic-start-address *model*) 2)
 
 load_address:
-    $01 $10 @(low basic_end) @(high basic_end) $01 $00 $9e @(princ main nil) 0
+    @(low (basic-start-address *model*))
+    @(high (basic-start-address *model*))
+    @(low basic_end)
+    @(high basic_end)
+    $01 $00 $9e @(princ main nil) 0
 basic_end:
     $00 $00
