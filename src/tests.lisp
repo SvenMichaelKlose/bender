b57674a72572edfe0f289fbd8bf3824665175c13
Sven Michael Klose <pixel@copei.de>
2015-11-07 14:37:39 +0100

    Also test assembler dumps.

--- src/tests.lisp
+++ src/tests.lisp
@@ -1,20 +1,21 @@
 ; bender – Copyright (c) 2015 Sven Michael Klose <pixel@copei.de>
 
-(format t "; Assembling reference 'tests/all_instructions.asm' to 'obj/all_instructions.bin'…~%")
+(defun test-compare-files (a b)
+  (| (string== (fetch-file a) (fetch-file b))
+     (error "Test failed! Files '~A' and '~A' differ." a b)))
+
 (assemble-files "obj/all_instructions.bin" "tests/all_instructions.asm")
+(test-compare-files "obj/all_instructions.bin"
+                    "tests/all_instructions.bin")
+(test-compare-files "obj/all_instructions.bin.lst"
+                    "tests/all_instructions.bin.lst")
 
-(format t "; Diassembling 'obj/all_instructions.bin' to 'obj/all_instructions_disassembled.lst'…")
 (disassemble-file "tests/all_instructions.bin" "obj/all_instructions_diassembled.lst")
-
-(= *model* :vic-20)
-(? (string== (fetch-file "obj/all_instructions.bin")
-             (fetch-file "tests/all_instructions.bin"))
-   (format t "; All instructions assembled correctly.~%")
-   (error "Instructions assembled incorrectly.~%"))
-
-(assemble-files "obj/cbm-basic-init.prg"
-                "vic-20/basic-loader.asm"
-                "growroom/cbm-basic-init.asm")
 
 (assemble-files "obj/inline-lisp.bin"
                 "tests/inline-lisp.asm")
+
+(with-temporary *model* :vic-20
+  (assemble-files "obj/cbm-basic-init.prg"
+                  "vic-20/basic-loader.asm"
+                  "growroom/cbm-basic-init.asm"))
