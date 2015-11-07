78ec8a76a6d5c15e8708af521067d0a85ea7a1ce
Sven Michael Klose <pixel@copei.de>
2015-11-07 02:26:42 +0100

    ASCII2PETSCII expects one or more strings.
    ASCII2PETSCII-CODE converts single chars.

--- src/ascii2petscii.lisp
+++ src/ascii2petscii.lisp
@@ -1,8 +1,9 @@
 ; Bender – Copyright (c) 2015 Sven Michael Klose <pixel@hugbox.org>
 
-(defun ascii2petscii (x)
-  (? (string? x)
-     (@ #'ascii2petscii (string-list x))
-     (? (< #\Z x)
-        (- x (-- #\a))
-        x)))
+(defun ascii2petscii-code (x)
+  (? (< #\Z x)
+     (- x (-- #\a))
+     x))
+
+(defun ascii2petscii (&rest x)
+  (list-string (@ #'ascii2petscii-code (string-list (apply #'+ x)))))
