13444294b18eae81d762178d9f02183685f35d29
Sven Michael Klose <pixel@copei.de>
2015-12-19 13:05:37 +0100

    Can translate pulse lengths to CPU cycles/s, e.g. to make VIC-20 TAP files that are going to be recorded with C64 tools and equipment for example.

--- src/tap/tap.lisp
+++ src/tap/tap.lisp
@@ -1,11 +1,19 @@
 ; bender – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
-(defun write-tap (o pulses)
-  (with-queue q
-    (alet (length pulses)
-      (format t "Writing TAP file...~%")
-      (format o "C64-TAPE-RAW")
-      (write-dword 1 o)
-      (write-dword ! o)
-      (format t "Writing ~A pulses...~%" !)
-      (princ pulses o))))
+(defun write-converted-tap (o pulses original-cycles converted-cycles)
+  (dotimes (i (length pulses))
+    (alet (integer (/ (* (elt pulses i) 8 converted-cycles) original-cycles))
+      (write-byte 0 o)
+      (write-byte (bit-and ! 255) o)
+      (write-byte (bit-and (>> ! 8) 255) o)
+      (write-byte (bit-and (>> ! 16) 255) o))))
+
+(defun write-tap (o pulses &key (original-cycles nil) (converted-cycles nil))
+  (alet (length pulses)
+    (format t "Writing TAP file of ~A pulses…~%" !)
+    (format o "C64-TAPE-RAW")
+    (write-dword 1 o)
+    (write-dword ! o)
+    (? converted-cycles
+       (write-converted-tap o pulses original-cycles converted-cycles)
+       (princ pulses o))))
