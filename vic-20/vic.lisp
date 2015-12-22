32d26e72b43de96578fed18d3b167ea676b57b28
Sven Michael Klose <pixel@copei.de>
2015-12-22 15:08:33 +0000

    Added functions to center screens of any dimensions.

--- vic-20/vic.lisp
+++ vic-20/vic.lisp
@@ -2,8 +2,19 @@
 
 (defconstant +vic-defaults-pal+
              '(#x0c #x26 #x16 #x2e #x00 #xc0 #x00 #x00
-               #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x1b)
+               #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x1b))
 
 (defconstant +vic-defaults-ntsc+
              '(#x05 #x19 #x16 #x2e #x00 #xc0 #x00 #x00
-               #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x1b)
+               #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x1b))
+
+(defun vic-defaults (tv)
+  (? (eq tv :ntsc)
+     +vic-defaults-ntsc+
+     +vic-defaults-pal+))
+
+(defun vic-horigin (tv num-columns)
+  (- (elt (vic-defaults tv) 0) (- 22 num-columns)))
+
+(defun vic-vorigin (tv num-rows)
+  (- (elt (vic-defaults tv) 0) (* 4 (- 22 num-rows))))
