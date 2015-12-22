b82f700e279488b6dcd0bc54c9a2476c54aa967d
Sven Michael Klose <pixel@copei.de>
2015-12-22 14:51:51 +0000

    +VIC-DEFAULTS-NTSC+ and +VIC-DEFAULTS-PAL+ added.

---
+++ vic-20/vic.lisp
@@ -0,0 +1,9 @@
+; Bender – Copyright (c) 2015 Sven Michael Klose <pixel@hugbox.org>
+
+(defconstant +vic-defaults-pal+
+             '(#x0c #x26 #x16 #x2e #x00 #xc0 #x00 #x00
+               #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x1b)
+
+(defconstant +vic-defaults-ntsc+
+             '(#x05 #x19 #x16 #x2e #x00 #xc0 #x00 #x00
+               #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x1b)
