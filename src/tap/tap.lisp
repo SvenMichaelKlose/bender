794df16b28f82367551b010fb14e2189022895a4
Sven Michael Klose <pixel@copei.de>
2016-01-22 17:04:04 +0100

    Can chose to write cycle exact values or 8-cycle chunks.

--- src/tap/tap.lisp
+++ src/tap/tap.lisp
@@ -1,12 +1,15 @@
 ; bender – Copyright (c) 2014–2015 Sven Michael Klose <pixel@copei.de>
 
-(defun write-converted-tap (o pulses original-cycles converted-cycles)
+(defun write-converted-tap (o pulses original-cycles converted-cycles &key (fine? nil))
   (dotimes (i (length pulses))
     (alet (integer (/ (* (elt pulses i) 8 converted-cycles) original-cycles))
-      (write-byte 0 o)
-      (write-byte (bit-and ! 255) o)
-      (write-byte (bit-and (>> ! 8) 255) o)
-      (write-byte (bit-and (>> ! 16) 255) o))))
+      (? fine?
+         (progn
+           (write-byte 0 o)
+           (write-byte (bit-and ! 255) o)
+           (write-byte (bit-and (>> ! 8) 255) o)
+           (write-byte (bit-and (>> ! 16) 255) o))
+         (write-byte (/ ! 8) o)))))
 
 (defun write-tap (o pulses &key (original-cycles nil) (converted-cycles nil))
   (alet (length pulses)
